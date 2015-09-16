7fe1fff6041c8d940d4cddcadafd2383d17edcc3
three0s <felix.y.tian@gmail.com>
2015-09-15 20:27:23 -0400

    Remove files not needed

--- GrimDawnAttributeResetTrainer/MyForm.cpp
+++
@@ -1,8 +0,0 @@
-#include "MyForm.h"
-using namespace GrimDawnAttributeResetTrainer;//使用MyForm.h中定义的命名空间
-[STAThread]//单线程特性，有些组件要求单线程
-int main(array<System::String^>^args){    
-	Application::EnableVisualStyles();//启用可视化界面，不然会很难看    
-	Application::Run(gcnew MyForm());//创建一个窗体，并进入消息循环    
-	return 0;
-}
